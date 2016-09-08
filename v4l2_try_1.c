e19ce83a5029edb315885540e1b297a1fdb5c1c6
cheensunj <354868819@qq.com>
2016-09-08 11:27:17 +0800

    have nothing to say

--- v4l2_try_1.c
+++ v4l2_try_1.c
@@ -1,10 +1,31 @@
 #include<unistd.h>
 #include<linux/videodev2.h>
-
-#define	DEV /dev/vedio0		//设备名
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/ioctl.h>
+#define	DEV "/dev/video0"		//设备名
 #define HEIGHT 640		//图像高度
 #define WIDTH 480		//图像宽度
 
+const char dev[]={"/dev/video0"};
 int main(int argc, char* argv[]){
-	
+	int fd;	
+	if((fd = open(dev,O_RDWR)) == -1){
+		printf("error: cannot open %s\n",dev);
+		return -1;
+	}
+	printf("fd is: %d\n",fd);
+	struct v4l2_capability cap;
+	if(ioctl(fd,VIDIOC_QUERYCAP,&cap ) == -1){
+		printf("error: cannot querycap --VIDIOC_QUERYCAP\n");
+	}
+	else {
+		printf("Driver: %s\n",cap.driver);
+		printf("Card: %s\n",cap.card);
+		printf("Bus_info: %d\n",cap.bus_info);
+		printf("Version: %d\n",cap.version);
+		printf("Capabilities: %x\n",cap.capabilities);
+	}
+
+	close(fd);
 }
