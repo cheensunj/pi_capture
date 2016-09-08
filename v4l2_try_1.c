9a7086fd8c939e8985523ed4b446f36ce87885d0
cheensunj <354868819@qq.com>
2016-09-08 21:54:04 +0800

    for VIDIOC_REQBUFS

--- v4l2_try_1.c
+++ v4l2_try_1.c
@@ -2,15 +2,15 @@
 #include<linux/videodev2.h>
 #include<stdio.h>
 #include<fcntl.h>
-#include<sys/ioctl.h>
+//#include<sys/ioctl.h>
 #define	DEV "/dev/video0"		//设备名
 #define HEIGHT 640		//图像高度
 #define WIDTH 480		//图像宽度
 
 const char dev[]={"/dev/video0"};
 int main(int argc, char* argv[]){
-	int fd;	
-	if((fd = open(dev,O_RDWR)) == -1){
+	const int fd  = open (dev,O_RDWR);	
+	if(fd == -1){
 		printf("error: cannot open %s\n",dev);
 		return -1;
 	}
@@ -18,6 +18,7 @@ int main(int argc, char* argv[]){
 	struct v4l2_capability cap;
 	if(ioctl(fd,VIDIOC_QUERYCAP,&cap ) == -1){
 		printf("error: cannot querycap --VIDIOC_QUERYCAP\n");
+		return -1;
 	}
 	else {
 		printf("Driver: %s\n",cap.driver);
@@ -26,6 +27,38 @@ int main(int argc, char* argv[]){
 		printf("Version: %d\n",cap.version);
 		printf("Capabilities: %x\n",cap.capabilities);
 	}
+	struct v4l2_fmtdesc fmtdesc;
+	fmtdesc.index = 0;
+	fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	while( ioctl(fd, VIDIOC_ENUM_FMT,&fmtdesc) != -1){
+		printf("index: %d  description: %s\n",fmtdesc.index,fmtdesc.description);
+		fmtdesc.index++;
+	}
+	struct v4l2_format format;
+	format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+	format.fmt.pix.height = HEIGHT;
+	format.fmt.pix.width = WIDTH;
+	format.fmt.pix.field = V4L2_FIELD_INTERLACED;
+	if(ioctl(fd, VIDIOC_S_FMT, &format) == -1){
+		printf("Unable to set format!  --VIDIOC_S_FMT\n");
+		return -1;
+	}
+	if(ioctl(fd, VIDIOC_G_FMT, &format) == -1){
+		printf("Unable to get format!  --VIDIOC_G_FMT\n");
+		return -1;
+	}
+	else {
+		//print format...
+	}
 
+	//Begin capture! ---------------------------------------------
+	struct v4l2_requestbuffers req;
+	req.count = 4;
+	req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	req.memory = V4L2_MEMORY_MMAP;
+	if(ioctl(fd,VIDIOC_REQBUFS,&req)==-1){
+		printf("request for buffers error\n");
+	}
 	close(fd);
 }
